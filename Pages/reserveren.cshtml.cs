8c4179032b7bb8e336c40d3a15dc3411d3e870ee
Giel Vaes <2403703vaes@zuyd.nl>
2025-04-04 13:03:43 +0200

    code geudate

---
+++ Pages/reserveren.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorApp.Pages
+{
+    public class reserverenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
