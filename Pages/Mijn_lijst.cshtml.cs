6aa6f4754f60a50116dbf08bd39fa61d931e7255
PC <PC@DESKTOP-KJIK9TK>
2025-04-11 15:47:28 +0200

    aantal html paginas toegevoegd met nog weinig functionaliteit

---
+++ Pages/Mijn_lijst.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorApp.Pages
+{
+    public class Mijn_lijstModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Profiel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorApp.Pages
+{
+    public class ProfielModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
